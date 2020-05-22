642ab4f5b194cba9bc74c0caed249cdca65a83f9
Juan Castillo <castillogo@hotmail.com>
2020-05-22 14:07:48 +0200

    commit without csv files

---
+++ exercisepy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 09:36:07 2020
+
+@author: casti
+"""
+
+list1 = [1, 2, 3, 4, 5, 6, 7]
+liststring = str(list1)
+
+#this is just to add somethind to the file
+
+print(list1)
+sumlist1 = sum(list1)
+print(sumlist1)
